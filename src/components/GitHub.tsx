40ee928aa5c8dad4e70a36a818336784939ad9ea
PavelSuchkov <suchkovp@gmail.com>
2021-09-16 18:53:58 +0300

    2

--- src/components/GitHub.tsx
+++ src/components/GitHub.tsx
@@ -12,30 +12,54 @@ type SearchUserType = {
 type SearchResult = {
   items: SearchUserType[]
 }
+type UserType = {
+    login: string
+    id: number
+    avatar_url: string
+    followers: string
+}
 
 export const GitHub = () => {
 
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
+    const [userDetails, setUserDetails] = useState<UserType | null>(null);
     const [users, setUsers] = useState<Array<SearchUserType>>([]);
+    const [tempSearch, setTempSearch] = useState<string>('it-kama');
+    const [searchTerm, setSearchTerm] = useState<string>('it-kama');
 
     useEffect(() => {
-        if(!!selectedUser){
+        if(selectedUser){
             document.title = selectedUser.login
         }
     },[selectedUser])
 
     useEffect(() => {
         axios
-            .get<SearchResult>(`https://api.github.com/search/users?q=it-kamasutra`)
+            .get<SearchResult>(`https://api.github.com/search/users?q=${searchTerm}`)
             .then(res => {
-               setUsers(res.data.items)
+                setUsers(res.data.items)
             })
-    }, [])
+    }, [searchTerm])
+
+    useEffect(() => {
+        if(!!selectedUser) {
+            axios
+                .get<UserType>(`https://api.github.com/users/${selectedUser.login}`)
+                .then(res => {
+                    setUserDetails(res.data)
+                })
+        }
+        console.log(userDetails)
+    }, [selectedUser])
+
+
     return (
         <div className={'container'}>
             <div>
                 <div>
-                    <input placeholder={'Search'}/> <button>Search</button>
+                    <input placeholder={'Search'} value={tempSearch}
+                    onChange={(e) => {setTempSearch(e.currentTarget.value)}}/>
+                    <button onClick={ () => setSearchTerm(tempSearch)}>Search</button>
                 </div>
                 <ul>
                     {users
@@ -45,8 +69,12 @@ export const GitHub = () => {
                 </ul>
             </div>
             <div>
-                <h2>{selectedUser}</h2>
-                <div>Details</div>
+                <h2>Username</h2>
+                {userDetails && <div>
+                    <img src={userDetails.avatar_url} alt=""/>
+                    <br/>
+                    {userDetails.login}, followers: {userDetails.followers}
+                </div>}
             </div>
         </div>
     )
